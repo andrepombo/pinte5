1dc709a59e2d9235523ebc8eb6a506453abefde4
lock-desk <pintepinturasdeveloper@gmail>
2025-01-16 19:36:31 -0300

    10000

--- vite-project/src/pages/tables/Tables.jsx
+++ vite-project/src/pages/tables/Tables.jsx
@@ -4,7 +4,7 @@ import { makeStyles } from "@mui/styles";
 //import MUIDataTable from "mui-datatables";
 
 // components
-import PageTitle from "../../components/PageTitle";
+import PageTitle from "../../components/PageTitle/PageTitle";
 // import Widget from "../../components/Widget";
 // import Table from "../dashboard/components/Table/Table";
 
